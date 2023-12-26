bb5fadbede4ce1c0f51316006e80b66071b627f8
liufufeng404 <135004836+liufufeng404@users.noreply.github.com>
2023-12-26 13:45:19 +0800

    Add files via upload

---
+++ px.py
@@ -0,0 +1,8 @@
+bot = '''
+perimeterx-bot
+keysword perimeterx px3 px2 pxvid pxhd
+
+https://www.ti.com https://www.flyfrontier.com/ https://www.textnow.com/ https://arcteryx.com/ https://https://portal.restaurant/
+
+Please contact us for cooperation: ichuan2062@gmail.com TG https://t.me/iMoFei
+'''
