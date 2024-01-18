f1855378957140e4a55ab788e40ab0333e1e4051
liufufeng404 <135004836+liufufeng404@users.noreply.github.com>
2024-01-18 11:05:08 +0800

    Update px.py

--- px.py
+++ px.py
@@ -2,7 +2,11 @@ bot = '''
 perimeterx-bot
 keysword perimeterx px3 px2 pxvid pxhd
 Please verify you are a human
-https://www.ti.com https://www.flyfrontier.com/ https://www.textnow.com/ https://arcteryx.com/ https://https://portal.restaurant/
-
+https://www.ti.com 
+https://www.flyfrontier.com/ 
+https://www.textnow.com/ 
+https://arcteryx.com/ 
+https://https://portal.restaurant/
+https://www.walmart.com/
 Please contact us for cooperation: ichuan2062@gmail.com TG https://t.me/iMoFei
 '''
